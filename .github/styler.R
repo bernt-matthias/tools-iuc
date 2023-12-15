242d88b75737a69ecbceffdb5f90d6c3713d9436
Matthias Bernt <m.bernt@ufz.de>
2023-12-15 18:09:29 +0100

    styler

---
+++ .github/styler.R
@@ -0,0 +1,13 @@
+library("argparse")
+library("styler")
+
+parser <- ArgumentParser(description = "Call styler")
+parser$add_argument("dir",
+  metavar = "DIR", type = "character",
+  help = "File to parse"
+)
+parser$add_argument("--dry",
+  choices = c("off", "on", "fail"), default = "on"
+)
+args <- parser$parse_args()
+result <- style_dir(args$dir, dry = args$dry)
